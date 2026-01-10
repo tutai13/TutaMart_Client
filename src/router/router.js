36f3632b0b70d2d0dad5756812b1cc6f05140b9e
tutai13 <taidtpd10687@gmail.com>
2026-01-11 04:01:20 +0700

    view order list

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,18 @@ import Cashier from "../components/Cashier.vue";
 import Dashboard from "../components/Dashboard.vue";
 import Inventory from "../components/Inventory.vue";
 import OrderList from "../components/OrderList.vue";
+import OrderDetail from "../components/OrderDetail.vue";
 const routes = [
   { path: "/", name: "Dashboard", component: Dashboard },
   { path: "/Cashier", name: "Cashier", component: Cashier },
   { path: "/Inventory", name: "Inventory", component: Inventory },
   { path: "/OrderList", name: "OrderList", component: OrderList },
+  {
+    path: "/OrderList/:id",
+    name: "OrderDetail",
+    component: OrderDetail,
+    props: true,
+  },
 ];
 const router = createRouter({
   history: createWebHistory(),
